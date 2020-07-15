1380fe1c8d7ce97d5a14081cb286c5c9041dddee
JVitor22 <jvfaria015@gmail.com>
2020-07-14 21:09:33 -0300

    services

--- src/services/CreateAppointment.ts
+++ src/services/CreateAppointment.ts
@@ -1,32 +1,31 @@
+import { startOfHour } from 'date-fns';
 import AppointmentRepository from '../repositories/AppointmentsRepository';
 import Appointment from '../models/Appointment';
-import { startOfHour, parseISO } from 'date-fns';
+
 interface AppointmentRequest {
   provider: string,
   date: Date,
-};
+}
 
 export default class CreateAppointment {
   private appointments: AppointmentRepository;
+
   constructor(appointments: AppointmentRepository) {
     this.appointments = appointments;
   }
 
-  public execute({provider, date}: AppointmentRequest): Appointment   {
+  public execute({ provider, date }: AppointmentRequest): Appointment {
     const parsedDate = startOfHour(date);
     const verify = this.appointments.verifyAppointment(parsedDate);
 
-
-  if (verify) {
-    throw Error("Could not create appointment in same date");
-  } else {
-    const appointment = this.appointments.create({
-      provider,
-      date: parsedDate
-    });
-    return appointment;
+    if (verify) {
+      throw Error('Could not create appointment in same date');
+    } else {
+      const appointment = this.appointments.create({
+        provider,
+        date: parsedDate,
+      });
+      return appointment;
+    }
   }
-
-  }
-
 }
