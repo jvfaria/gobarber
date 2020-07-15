5905e3defed70f472a0760f9b785ef37bb725540
JVitor22 <jvfaria015@gmail.com>
2020-07-14 21:10:05 -0300

    src-server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import express from 'express';
-import routes from './routes/index'
+import routes from './routes/index';
+
 const app = express();
 app.use(express.json());
 app.use(routes);
 
-
-app.listen(3333 , () => {
-  console.log("Skull is running on port: 3333");
-
+app.listen(3333, () => {
+  console.log('Skull is running on port: 3333');
 });
 
 export default app;
