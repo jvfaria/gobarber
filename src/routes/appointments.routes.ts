07e88f4d1e5bc158f3f266ca9512c3b9b9c67656
JVitor22 <jvfaria015@gmail.com>
2020-07-15 11:39:09 -0300

    routes

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,28 +1,43 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 import CreateAppointment from '../services/CreateAppointment';
-import AppointmentRepository from '../repositories/AppointmentsRepository';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-const appointments = new AppointmentRepository();
 
-appointmentsRouter.get('/', (request, response) =>
-  response.json(appointments.listAppointments()),
-);
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.get('/', async (request, response) => {
+  try {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    const foundedAppointments = await appointmentsRepository.find();
+    const appointmentsOrNot =
+      foundedAppointments.length < 1
+        ? { message: 'You have any appointments booked' }
+        : foundedAppointments;
+
+    return response.status(200).json(appointmentsOrNot);
+  } catch (error) {
+    return response
+      .status(400)
+      .json(`${error.message} you have no appointments`);
+  }
+});
+
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
     const parsedDate = parseISO(date);
-    const appointmentService = new CreateAppointment(appointments);
 
-    const appointment = appointmentService.execute({
+    const appointmentService = new CreateAppointment();
+
+    const appointment = await appointmentService.execute({
       provider,
       date: parsedDate,
     });
 
     return response.status(201).json(appointment);
   } catch (error) {
-    return response.status(400).json(error.message);
+    return response.status(400).json({ message: error.message });
   }
 });
 
