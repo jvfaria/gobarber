f47e154a4a6cd3a8b758d34a84a3089cffc04eb7
JVitor22 <jvfaria015@gmail.com>
2020-07-19 13:57:45 -0300

    return-user-token

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -7,9 +7,9 @@ sessionsRouter.post('/', async (request, response) => {
   try {
     const { email, password } = request.body;
     const authService = new AuthUserService();
-    const auth = await authService.execute({ email, password });
+    const { user, token } = await authService.execute({ email, password });
 
-    return response.status(200).json(auth);
+    return response.status(200).json({ user, token });
   } catch (error) {
     return response.status(400).json({ message: error.message });
   }
